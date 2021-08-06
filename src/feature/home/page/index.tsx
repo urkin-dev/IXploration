61548480b54b57f0b43ac9a6c0932029906ab62b
Ivan <yurkin.ivan5456@gmail.com>
2021-08-07 01:15:55 +0500

    Fix scroll view on Home page

--- src/feature/home/page/index.tsx
+++ src/feature/home/page/index.tsx
@@ -2,7 +2,7 @@ import { IHomeNavigationProp } from '@feature/app'
 import { RoutesEnum } from 'feature/app/lib/Routes'
 import React from 'react'
 import { useRef } from 'react'
-import { NativeSyntheticEvent, ScrollView, StyleSheet, TextInputFocusEventData } from 'react-native'
+import { NativeSyntheticEvent, ScrollView, StyleSheet, TextInputFocusEventData, View } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 import { SectionTitleContainer } from 'ui'
 import ApartmentCard from './components/ApartmentCard'
@@ -12,51 +12,53 @@ interface IProps extends IHomeNavigationProp {}
 
 const HomePage = ({ route, navigation }: IProps) => {
 	return (
-		<ScrollView style={styles.HomeContainer}>
-			<TextInput style={styles.StyledSearch} placeholder="Find rent house near you" />
-			<Slider />
-			<SectionTitleContainer
-				style={{ marginTop: 30, marginBottom: 20 }}
-				title="Recommended For You"
-				linkText="View All"
-				linkRef={RoutesEnum.Recommended}
-			/>
-			<ApartmentCard
-				title="Some title"
-				subtitle="Some subtitle"
-				price={345}
-				pricePrefix="$"
-				reviewValue={5}
-				isLastChild={false}
-				horizontal
-			/>
-			<ApartmentCard
-				title="Some title"
-				subtitle="Some subtitle"
-				price={345}
-				pricePrefix="$"
-				reviewValue={5}
-				isLastChild={false}
-				horizontal
-			/>
-			<ApartmentCard
-				title="Some title"
-				subtitle="Some subtitle"
-				price={345}
-				pricePrefix="$"
-				reviewValue={5}
-				isLastChild={false}
-				horizontal
-			/>
-			<ApartmentCard
-				title="Some title"
-				subtitle="Some subtitle"
-				price={345}
-				pricePrefix="$"
-				reviewValue={5}
-				isLastChild
-				horizontal
-			/>
+		<ScrollView style={StyleSheet.absoluteFill}>
+			<View style={styles.HomeContainer}>
+				<TextInput style={styles.StyledSearch} placeholder="Find rent house near you" />
+				<Slider />
+				<SectionTitleContainer
+					style={{ marginTop: 30, marginBottom: 20 }}
+					title="Recommended For You"
+					linkText="View All"
+					linkRef={RoutesEnum.Recommended}
+				/>
+				<ApartmentCard
+					title="Some title"
+					subtitle="Some subtitle"
+					price={345}
+					pricePrefix="$"
+					reviewValue={5}
+					isLastChild={false}
+					horizontal
+				/>
+				<ApartmentCard
+					title="Some title"
+					subtitle="Some subtitle"
+					price={345}
+					pricePrefix="$"
+					reviewValue={5}
+					isLastChild={false}
+					horizontal
+				/>
+				<ApartmentCard
+					title="Some title"
+					subtitle="Some subtitle"
+					price={345}
+					pricePrefix="$"
+					reviewValue={5}
+					isLastChild={false}
+					horizontal
+				/>
+				<ApartmentCard
+					title="Some title"
+					subtitle="Some subtitle"
+					price={345}
+					pricePrefix="$"
+					reviewValue={5}
+					isLastChild
+					horizontal
+				/>
+			</View>
 		</ScrollView>
 	)
 }
